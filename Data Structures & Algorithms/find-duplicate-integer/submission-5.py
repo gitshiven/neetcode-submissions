114f93bc476f3c59dd7ca2821a37b85e637defad
Shiven Singh <ss.shiven44@gmail.com>
2026-08-11 18:29:19 +0100

    Add: find-duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        #Floyd's Cycle Detection
+        slow = fast = 0
+        #Cycle dhundo
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow_2 = 0
+        while True:
+            if slow==slow_2:
+                break
+            slow_2 = nums[slow_2]
+            slow = nums[slow]
+    
+        return slow 
